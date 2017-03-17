7cf2ee38fd3082ce6747c95815a041731355d26d
Unknown <sbahn1992@gmail.com>
2017-03-17 13:37:07 +0900

    Init function pointer

--- interpreter/interpreter.c
+++ interpreter/interpreter.c
@@ -14,8 +14,75 @@ static bool is_running = true;
 
 void usageExit() {
     // TODO: show usage
+    printf(" Usage :./interpreter <File> \n");
     exit(1);
 }
+
+void *halt (struct VMcontext* ctx, const uint32_t instr){
+	
+	printf("Terminate the proccess  \n");
+}
+
+void *load (struct VMcontext* ctx, const uint32_t instr){
+
+        printf("Terminate the proccess  \n");
+}
+
+void *store (struct VMcontext* ctx, const uint32_t instr){
+
+        printf("Terminate the proccess  \n");
+}
+
+void *move (struct VMcontext* ctx, const uint32_t instr){
+
+        printf("Terminate the proccess  \n");
+	}
+
+void *puti (struct VMcontext* ctx, const uint32_t instr){
+
+        printf("Terminate the proccess  \n");
+	}
+
+void *add (struct VMcontext* ctx, const uint32_t instr){
+
+        printf("Terminate the proccess  \n");
+	}
+void *sub (struct VMcontext* ctx, const uint32_t instr){
+
+        printf("Terminate the proccess  \n");
+	}
+
+void *gt (struct VMcontext* ctx, const uint32_t instr){
+
+        printf("Terminate the proccess  \n");
+	}
+
+void *ge (struct VMcontext* ctx, const uint32_t instr){
+
+        printf("Terminate the proccess  \n");
+	}
+void *eq (struct VMcontext* ctx, const uint32_t instr){
+
+        printf("Terminate the proccess  \n");
+	}
+void *ite (struct VMcontext* ctx, const uint32_t instr){
+
+        printf("Terminate the proccess  \n");
+	}
+
+void *jump (struct VMcontext* ctx, const uint32_t instr){
+
+        printf("Terminate the proccess  \n");
+	}
+void *puts (struct VMcontext* ctx, const uint32_t instr){
+
+        printf("Terminate the proccess  \n");
+	}
+void *gets (struct VMcontext* ctx, const uint32_t instr){
+
+        printf("Terminate the proccess  \n");
+	}
+
 
 void initFuncs(FunPtr *f, uint32_t cnt) {
     uint32_t i;
@@ -24,8 +91,21 @@ void initFuncs(FunPtr *f, uint32_t cnt) {
     }
 
     // TODO: initialize function pointers
-    // f[0x00] = halt;
-    // f[0x10] = load;
+     f[0x00] = halt;
+     f[0x10] = load;
+     f[0x20] = store;
+     f[0x30] = move;
+     f[0x40] = puti;
+     f[0x50] = add;
+     f[0x60] = sub;
+     f[0x70] = gt;
+     f[0x80] = ge;
+     f[0x90] = eq;
+     f[0xa0] = ite;
+     f[0xb0] = jump;
+     f[0xc0] = puts;
+     f[0xd0] = gets;
+    
 }
 
 void initRegs(Reg *r, uint32_t cnt)
